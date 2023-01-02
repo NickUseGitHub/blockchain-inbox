9ba4e32ba6a662e4e6ec1db393f189c066198e12
Chaiwat Tungtongsoontorn <chaiwat_t@tripetch-it.co.th>
2023-01-02 15:03:09 +0700

    test: init Inbox.test

---
+++ test/Inbox.test.js
@@ -0,0 +1,32 @@
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
+const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require("../compile");
+
+const INITIAL_MESSAGE = "Hi there!";
+let accounts;
+let inbox;
+
+beforeEach(async function () {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: [INITIAL_MESSAGE],
+    })
+    .send({ from: accounts[0], gas: "1000000" });
+});
+
+describe("Inbox", function () {
+  it("deploys a contract", function () {
+    assert.ok(inbox.options.address);
+  });
+
+  it("has a default message", async function () {
+    const msg = await inbox.methods.message().call();
+
+    assert.equal(msg, INITIAL_MESSAGE);
+  });
+});
